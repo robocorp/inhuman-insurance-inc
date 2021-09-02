1afc86a9aef26ddceb10ff1aa7b2727c80af94dd
Jani Palsamäki <jani@robocorp.com>
2021-09-02 15:34:51 +0300

    Test response

--- sales-system-api.js
+++ sales-system-api.js
@@ -2,13 +2,7 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 console.log(urlParams);
-const request = new XMLHttpRequest();
-request.open(
-  'GET',
-  'https://robocorp.github.io/inhuman-insurance-inc/RS_246.json'
-);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-  print(request.response);
-};
+const response = new Response();
+response.type = 'application/json';
+response.body = '{"key":"value"}';
+print(response);
